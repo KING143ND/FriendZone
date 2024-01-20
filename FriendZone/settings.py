199b649669e816a568ac55d306b9d89739ca0a37
KING143ND <nvnaveenchaudhary@gmail.com>
2024-01-20 16:15:50 +0530

    FriendZone v-1.0

--- FriendZone/settings.py
+++ FriendZone/settings.py
@@ -88,9 +88,18 @@ WSGI_APPLICATION = 'FriendZone.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'Ndnaveen@143',
+       'HOST': 'db.huobdsadxflsbfnegnal.supabase.co',
+       'PORT': '5432',
+       'CONN_MAX_AGE': 900,
     }
 }
 
